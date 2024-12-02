401b418cc5f6b14062957ac313c62e9884faeeac
Gabriel Carlos <gabrielcarlos0705@gmail.com>
2024-12-02 02:22:17 -0300

    feat: Mappers

--- api/Controllers/StockController.cs
+++ api/Controllers/StockController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using api.Data;
+using api.Mappers;
 using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 
@@ -19,8 +20,9 @@ namespace api.Controllers
         [HttpGet]
         public IActionResult GetAll()
         {
-            var stocks = _context.Stocks.ToList();
-            
+            var stocks = _context.Stocks.ToList()
+            .Select(stock => stock.ToStockDto());
+
             return Ok(stocks);
         }
 
@@ -33,7 +35,7 @@ namespace api.Controllers
             {
                 return NotFound();
             }
-            return Ok(stock);
+            return Ok(stock.ToStockDto());
         }
     }
 }
