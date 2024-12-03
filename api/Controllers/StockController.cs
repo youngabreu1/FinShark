d2c785dcc863c479397354a840bee03aaeb97f1f
Gabriel Carlos <gabrielcarlos0705@gmail.com>
2024-12-03 01:26:34 -0300

    feat: http post to stocks

--- api/Controllers/StockController.cs
+++ api/Controllers/StockController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using api.Data;
+using api.Dtos.Stock;
 using api.Mappers;
 using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
@@ -36,6 +37,15 @@ namespace api.Controllers
                 return NotFound();
             }
             return Ok(stock.ToStockDto());
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromBody] CreateStockRequestDto stockDto)
+        {
+           var stockModel = stockDto.ToStockFromCreateDTO();
+           _context.Stocks.Add(stockModel);
+           _context.SaveChanges();
+           return CreatedAtAction(nameof(GetById), new { id = stockModel.Id }, stockModel.ToStockDto());
         }
     }
 }
